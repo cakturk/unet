f5f6510cef7e8835723a5dea039928eb9fbda85e
Cihangir Akturk <cakturk@gmail.com>
2016-12-27 11:27:35 +0300

    mbuf: poor man's mbuf memory pool
    Signed-off-by: Cihangir Akturk <cakturk@gmail.com>

--- mbuf.c
+++ mbuf.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "mbuf.h"
 
+#define MBUF_POOL_LEN 16
+
+static struct mbuf mpool[MBUF_POOL_LEN];
+static struct mbuf *free_list;
+
 struct mbuf *mb_alloc(void)
 {
 	struct mbuf *m;
@@ -14,4 +19,70 @@ struct mbuf *mb_alloc(void)
 void mb_free(struct mbuf *m)
 {
 	free(m);
+}
+
+void mb_pool_init(void)
+{
+	int i;
+
+	free_list = &mpool[0];
+	for (i = 1; i < sizeof(mpool) / sizeof(mpool[0]); ++i)
+		mpool[i - 1].m_next = &mpool[i];
+	mpool[MBUF_POOL_LEN - 1].m_next = NULL;
+}
+
+static inline struct mbuf *mb_pool_alloc(void)
+{
+	struct mbuf *m = NULL;
+
+	if (free_list) {
+		m = free_list;
+		free_list = m->m_next;
+		mb_init(m);
+	}
+
+	return m;
+}
+
+struct mbuf *mb_pool_chain_alloc(unsigned int nrbufs)
+{
+	struct mbuf *m, **curr = &free_list;
+
+	if (!*curr)
+		return NULL;
+
+	while (nrbufs--) {
+		(*curr)->m_head = (*curr)->m_data;
+		(*curr)->m_tail = (*curr)->m_data;
+		curr = &(*curr)->m_next;
+
+		if (!*curr)
+			break;
+	}
+
+	m = free_list;
+	free_list = *curr;
+	*curr = NULL;
+
+	return m;
+}
+
+static inline void mb_pool_free(struct mbuf *m)
+{
+	m->m_next = free_list;
+	free_list = m;
+}
+
+void mbuf_link(struct mbuf **head, struct mbuf *first, struct mbuf *last)
+{
+	last->m_next = *head;
+	*head = first;
+}
+
+struct mbuf *mbuf_unlink(struct mbuf **first, struct mbuf **last)
+{
+	struct mbuf *m = *first;
+	*first = (*last)->m_next;
+	*last = NULL;
+	return m;
 }
